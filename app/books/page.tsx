d48a2fa8aa14b51031514f8ab5ca9058618cdd80
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-10-20 17:11:28 +0300

    added global styles and others

---
+++ app/books/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import Button from "antd/es/button/button"
+
+export default function BooksPage() {
+    return (
+        <div>
+            <Button></Button>
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import { Menu } from "antd";
 import "./globals.css";
+import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
+import Link from "next/link";
+
+const items = [
+  {key: "home" , label: <Link href={"/"}>Home</Link>}, 
+  {key: "books" , label: <Link href={"/books"}>Books</Link>}]
+  
 
 export default function RootLayout({
   children,
@@ -8,7 +16,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Layout style={{minHeight: "100vh"}}>
+          <Header>
+            <Menu
+              theme="dark"
+              mode ="horizontal"
+              items = {items}
+              style={{flex:1 ,minWidth: 0}}
+              />
+          </Header>
+            <Content style={{padding: "0 48px"}}>{children}</Content>
+            <Footer style={{textAlign: "center"}}>
+              BookStore 2024
+            </Footer>
+        </Layout>
       </body>
     </html>
   );
