1e585de57411c3080bbc7e1919551ab6e7835ed3
Ігор Колобилін <igor.kolobulin@gmail.com>
2024-10-21 16:23:37 +0300

    logic and style are done

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,10 +1,92 @@
 "use client"
 import Button from "antd/es/button/button"
+import { Books } from "../components/Books"
+import { useEffect, useState } from "react"
+import { BookRequest, createBook, deleteBook, getAllBooks, updateBook } from "../services/books"
+import Title from "antd/es/typography/Title"
+import { CreateUpdateBook, Mode } from "../components/CreateUpdateBook"
+import { tree } from "next/dist/build/templates/app-page"
 
 export default function BooksPage() {
+    const defaultValues = {
+        title: "",
+        description: "",
+        price: 1,
+    } as Book;
+
+    const [values,setValues] = useState<Book>(defaultValues);
+
+    const [books, setBooks] = useState<Book[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [mode, setMode] = useState(Mode.Create);
+
+    useEffect(()=> {
+        const getBooks = async () => {
+            const books = await getAllBooks();
+            setLoading(false);
+            setBooks(books);
+        };
+        getBooks();
+    },[])
+
+    const handleCreateBook = async (request: BookRequest) => {
+        await createBook(request);
+        closeModal();
+
+        const books = await getAllBooks();
+        setBooks(books);
+    };
+    
+    const handleUpdateBook = async (id: string, request: BookRequest) => {
+        await updateBook(id, request);
+        closeModal();
+
+        const books = await getAllBooks();
+        setBooks(books);
+    };
+
+    const openEditModal = async (book: Book) => {
+        setMode(Mode.Edit);
+        setValues(book);
+        setIsModalOpen(true);
+    }
+    
+    const handleDeleteBook = async (id: string) => {
+        await deleteBook(id);
+        closeModal();
+
+        const books = await getAllBooks();
+        setBooks(books);
+    };
+    
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+    
+    const closeModal = () => {
+        setValues(defaultValues);
+        setIsModalOpen(false);
+    };
+
     return (
         <div>
-            <Button></Button>
+            <Button 
+                type="primary"
+                style={{marginTop: "30px"}}
+                size="large"
+                onClick={openModal}>Add book
+            </Button>
+
+            <CreateUpdateBook 
+                mode={mode} 
+                values={values} 
+                isModalOpen={isModalOpen} 
+                handleCreate={handleCreateBook} 
+                handleUpdate={handleUpdateBook} 
+                handleCancel={closeModal}
+            />
+            {loading ? <Title>Loading....</Title> : <Books books={books} handOpen={openEditModal} handleDelete={handleDeleteBook}/>}
         </div>
-    )
+    );
 }
--- app/components/Books.tsx
+++ app/components/Books.tsx
@@ -3,10 +3,12 @@ import { CardTitle } from "./Cardtitle"
 import Button from "antd/es/button/button"
 
 interface Props {
-    books: Book[]
+    books: Book[];
+    handleDelete: (id:string) => void;
+    handOpen: (book: Book) => void;
 }
 
-export const Books = ({books}: Props) => {
+export const Books = ({books, handleDelete, handOpen}: Props) => {
     return (
         <div className="cards">
             {books.map((book : Book) => (
@@ -16,9 +18,16 @@ export const Books = ({books}: Props) => {
                     bordered={false}
                 >
                     <p>{book.description}</p>
-                    <div className="card__buttons">
-                        <Button>Edit</Button>
-                        <Button>Delete</Button>
+                    <div className="card_buttons">
+                        <Button 
+                            onClick={()=> handOpen(book)} 
+                            style={{flex:1}}>Edit
+                        </Button>
+                        <Button 
+                            onClick={()=> handleDelete(book.id)}
+                            danger
+                            style={{flex:1}}>Delete
+                        </Button>
                     </div>
                 </Card>
             ))}
